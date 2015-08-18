d2a454341b60cacf43f2961ad37572810c9d23c5
Sashko Stubailo <sashko@stubailo.com>
2015-08-17 19:24:00 -0700

    Use run instead of await

--- examples/react-todos/lib/lists.js
+++ examples/react-todos/lib/lists.js
@@ -6,13 +6,13 @@ if (Meteor.isClient) {
 
 Meteor.methods({
   '/lists/add': function () {
-    return PG.await(PG.knex("lists").insert({}).returning("id"))[0];
+    return PG.knex("lists").insert({}).returning("id").run()[0];
   },
   '/lists/updateName': function (listId, newName) {
-    PG.await(PG.knex("lists").update({name: newName}).where({id: listId}));
+    PG.knex("lists").update({name: newName}).where({id: listId}).run();
   },
   '/lists/togglePrivate': function (listId) {
-    var list = PG.await(PG.knex("lists").where({id: listId}))[0];
+    var list = PG.knex("lists").where({id: listId}).run()[0];
 
     if (! Meteor.userId()) {
       throw new Meteor.Error("not-logged-in");
@@ -21,38 +21,38 @@ Meteor.methods({
     console.log(list);
     if (list.user_id) {
       console.log("setting user id to null");
-      PG.await(PG.knex("lists").where({id: listId}).update({user_id: null}));
+      PG.knex("lists").where({id: listId}).update({user_id: null}).run();
     } else {
       // ensure the last public list cannot be made private
-      if (! list.user_id && PG.await(PG.knex("lists").count("*").whereNull("user_id"))[0].count == 1) {
+      if (! list.user_id && PG.knex("lists").count("*").whereNull("user_id").run()[0].count == 1) {
         throw new Meteor.Error("final-list-private");
       }
 
-      PG.await(PG.knex("lists").where({id: listId}).update({user_id: Meteor.userId()}));
+      PG.knex("lists").where({id: listId}).update({user_id: Meteor.userId()}).run();
     }
   },
   '/lists/delete': function (listId) {
-    var list = PG.await(PG.knex("lists").where({id: listId}))[0];
+    var list = PG.knex("lists").where({id: listId}).run()[0];
 
     // ensure the last public list cannot be deleted.
-    if (! list.user_id && PG.await(PG.knex("lists").count("*").whereNull("user_id"))[0].count == 1) {
+    if (! list.user_id && PG.knex("lists").count("*").whereNull("user_id").run()[0].count == 1) {
       throw new Meteor.Error("final-list-delete");
     }
 
     PG.inTransaction(() => {
       // Make sure to delete all of the items
-      PG.await(PG.knex("todos").where({list_id: listId}).delete());
+      PG.knex("todos").where({list_id: listId}).delete().run();
 
       // Delete the list itself
-      PG.await(PG.knex("lists").where({id: listId}).delete());
+      PG.knex("lists").where({id: listId}).delete().run();
     });
   },
   '/lists/addTask': function (listId, newTaskText) {
-    PG.await(PG.knex("todos").insert({
+    PG.knex("todos").insert({
       list_id: listId,
       text: newTaskText,
       checked: false
-    }));
+    }).run();
 
     // Lists.update(listId, {$inc: {incompleteCount: 1}});
   }
--- examples/react-todos/lib/todos.js
+++ examples/react-todos/lib/todos.js
@@ -1,25 +1,18 @@
+// Stuff below here is server-only
+Todos = new PG.Table('todos');
+
 if (Meteor.isClient) {
-  // Stuff below here is server-only
-  Todos = new PG.Table('todos');
   return;
 }
 
 Meteor.methods({
   '/todos/delete': function (todoId) {
-    PG.await(PG.knex("todos").delete().where({id: todoId}));
-
-    // We would want the below for optimistic UI
-    // if (! todo.checked) {
-    //   Lists.update(todo.listId, {$inc: {incompleteCount: -1}});
-    // }
+    Todos.knex.delete().where({id: todoId}).run();
   },
   '/todos/setChecked': function (todoId, checked) {
-    PG.await(PG.knex("todos").update({checked: checked}).where({id: todoId}));
-
-    // We would want the below for optimistic UI
-    // Lists.update(todo.listId, {$inc: {incompleteCount: checked ? -1 : 1}});
+    Todos.knex.update({checked: checked}).where({id: todoId}).run();
   },
   '/todos/setText': function (todoId, newText) {
-    PG.await(PG.knex("todos").update({text: newText}).where({id: todoId}));
+    Todos.knex.update({text: newText}).where({id: todoId}).run();
   }
 });
